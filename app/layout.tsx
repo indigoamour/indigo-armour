0ce27f20c23d754044a519fd0f880c44bd4ecc5e
Pulkit Porwal <pulkitporwal.brn@gmail.com>
2025-08-20 21:27:17 +0530

    Google Analytics Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,12 +39,27 @@ fbq('track', 'PageView');
           `}
         </Script>
 
+        {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-PZ9B2D3J');`}
+        </Script>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JB84JG50XR"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JB84JG50XR');
+          `}
         </Script>
 
         <title>Indigo Amour</title>
@@ -68,12 +83,12 @@ fbq('track', 'PageView');
             src="https://www.facebook.com/tr?id=1453290402356595&ev=PageView&noscript=1"
           />
         </noscript>
-         <noscript>
+        <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PZ9B2D3J"
             height="0"
             width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
+            style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
         <ModalProvider />
